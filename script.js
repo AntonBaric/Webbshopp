2f4f5bd1bdf94c57b6f23ce0f027c2835b4eb52a
Anton Baric <anton.baric99@gmail.com>
2020-10-15 23:42:04 +0200

    Added article-style.css, modified a lot

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function addToCart() {
     alert('added to cart')
 }
+
+const searchInput = document.getElementById("search")
+
+searchInput.addEventListener ("input", () => {
+    console.log("input event fired");
+});
